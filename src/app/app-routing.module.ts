08104ad8130cb97ed0ee8b47c0e46b28d488a8e5
Victor Abraham Bernedo Tapia <host29.3@gmail.com>
2022-12-06 01:12:13 -0500

    Info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthRoutingModule } from './auth/auth-routing.module';
+import { PopUpSuccess } from './InfoPopUs/popupsuccess/popupsuccess.component';
 import { NopageFoundComponent } from './nopage-found/nopage-found.component';
 import { PagesRoutingModule } from './pages/pages--routing.module';
 
 
 const routes: Routes = [
   {path: '', redirectTo:'/login',pathMatch:'full'},
+  {path: 'info', component:PopUpSuccess},
   {path: '**',component:NopageFoundComponent},
   
 ];
